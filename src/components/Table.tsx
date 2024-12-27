a341894db7d8e395295d6e3e821752853e99c894
Leandro Rey <leandrodrey@gmail.com>
2024-12-26 22:30:42 -0300

    Style Table component with consistent padding and borders.

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -17,19 +17,19 @@ interface ColumnConfig<T> {
 
 const Table = <T extends BaseTableItem>({ items, columns }: TableProps<T>) => {
     return (
-        <table>
+        <table className="text-left">
             <thead>
             <tr>
                 {columns.map((column) => (
-                    <th key={String(column.key)}>{column.header}</th>
+                    <th key={String(column.key)} className="p-3">{column.header}</th>
                 ))}
             </tr>
             </thead>
             <tbody>
             {items.map((item) => (
-                <tr key={item.id}>
+                <tr key={item.id} className="border-b border-gray-200 ">
                     {columns.map((column) => (
-                        <td key={String(column.key)}>
+                        <td key={String(column.key)} className="p-3">
                             {column.render ? (
                                 column.render(item[column.key], item)
                             ) : (
