79f6ff6f56126e465d91f648a15ddb00a675b7e4
Leandro Rey <leandrodrey@gmail.com>
2024-12-27 19:11:35 -0300

    Add column sorting functionality to Table component
    Introduce `useSortTable` hook to enable sorting on table columns. Updated columns to include a `sortable` property and adjusted headers for click-based sorting. Display sorting indicators (▲/▼) alongside active column headers.

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,4 +1,5 @@
-import {ReactNode} from "react";
+import {ReactNode, useState} from "react";
+import useSortTable from "../hooks/useSortTable.tsx";
 
 interface BaseTableItem {
     id: string | number;
@@ -13,21 +14,37 @@ interface ColumnConfig<T> {
     header: string;
     key: keyof T;
     render?: (value: any, item: T) => ReactNode;
+    sortable?: boolean;
 }
 
-const Table = <T extends BaseTableItem>({ items, columns }: TableProps<T>) => {
+const Table = <T extends BaseTableItem>({items, columns}: TableProps<T>) => {
+
+    const { sortedItems, sortKey, sortOrder, handleHeaderClick } = useSortTable({ items });
+
     return (
         <table className="text-left">
             <thead>
             <tr>
                 {columns.map((column) => (
-                    <th key={String(column.key)} className="p-3">{column.header}</th>
+                    <th
+                        key={String(column.key)}
+                        className="p-3"
+                        onClick={() => handleHeaderClick(column.key, column.sortable ?? true)}
+                        style={{cursor: column.sortable ? 'pointer' : 'default'}}
+                    >
+                        {column.header}
+                        {sortKey === column.key && (
+                            <span className="ml-2">
+                                {sortOrder === 'asc' ? '▲' : '▼'}
+                            </span>
+                        )}
+                    </th>
                 ))}
             </tr>
             </thead>
             <tbody>
-            {items.map((item) => (
-                <tr key={item.id} className="border-b border-gray-200 ">
+            {sortedItems.map((item) => (
+                <tr key={item.id} className="border-b border-gray-200">
                     {columns.map((column) => (
                         <td key={String(column.key)} className="p-3">
                             {column.render ? (
