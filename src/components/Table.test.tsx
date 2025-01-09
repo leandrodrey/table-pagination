ca43707c5a7a80532e62c4ad8dd2f366e731a739
Leandro Rey <leandrodrey@gmail.com>
2025-01-09 20:00:56 -0300

    Refactor test and add row render verification in Table.
    Renamed column header "Portrait" to "Image" for clarity. Added a new test case to validate table row count and ensure each cell contains the correct data based on mock inputs. Enhanced import statements for additional testing utilities.

--- src/components/Table.test.tsx
+++ src/components/Table.test.tsx
@@ -1,4 +1,4 @@
-import {render, screen} from '@testing-library/react';
+import {render, screen, within} from '@testing-library/react';
 import Table from './Table';
 import {ICharacterTable} from "../interfaces/ICharacterTable.ts";
 import {IColumnConfig} from "../interfaces/IColumnConfig.ts";
@@ -15,7 +15,7 @@ describe('Table', () => {
         {header: 'Species', key: 'species', sortable: true},
         {header: 'Status', key: 'status', sortable: true},
         {
-            header: 'Portrait',
+            header: 'Image',
             key: 'imageUrl',
             render: (imageUrl) => <span data-testid="custom-component">{imageUrl}</span>,
             sortable: false,
@@ -26,6 +26,38 @@ describe('Table', () => {
         render(<Table items={mockItems} columns={mockColumns}/>);
         mockColumns.forEach((column) => {
             expect(screen.getByRole('columnheader', {name: column.header})).toBeInTheDocument();
+        });
+    });
+
+    it('should render the table with the correct number of rows and data', () => {
+        render(<Table items={mockItems} columns={mockColumns} />);
+        const tableRows = screen.getAllByRole('row');
+
+        // 1 header row + 3 data rows = 4 rows
+        expect(tableRows).toHaveLength(4);
+
+        // Iterate over the data rows (ignoring the header row)
+        tableRows.slice(1).forEach((row, rowIndex) => {
+            const cells = within(row).getAllByRole('cell');
+            expect(cells).toHaveLength(mockColumns.length);
+
+            // Verify the data in each cell
+            cells.forEach((cell, cellIndex) => {
+                const column = mockColumns[cellIndex];
+
+                // Obtain the expected value
+                const expectedValue = column.render
+                    ? column.render(mockItems[rowIndex][column.key], mockItems[rowIndex])
+                    : String(mockItems[rowIndex][column.key]);
+
+                // Verify if the cell contains the expected value (as text or as an element)
+                if (typeof expectedValue === 'string') {
+                    expect(cell).toHaveTextContent(expectedValue);
+                } else {
+                    // If expectedValue is a React element, verify if it is present in the cell
+                    expect(within(cell).getByTestId('custom-component')).toBeInTheDocument();
+                }
+            });
         });
     });
 
